f1a3822815d5da188f50f360a3df5be6b717be84
Alfonso-Chiguay <alfonso.chiuay@gmail.com>
2020-07-03 14:30:29 -0200

    Se agregan mas ventanas a las vistas

---
+++ BeLife/Vistas/AdministrarCliente.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Drawing;
+
+namespace BeLife.Vistas
+{
+    /// <summary>
+    /// Lógica de interacción para AdministrarCliente.xaml
+    /// </summary>
+    /// 
+    
+
+    public partial class AdministrarCliente : Window
+    {
+        public AdministrarCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_salir_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txt_rut_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (txt_rut.Text.Equals("Ej: 12345678"))
+            {
+                txt_rut.Text = "";
+                
+                    
+            }
+                
+
+        }
+
+        private void txt_rut_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (txt_rut.Text.Equals(""))
+                txt_rut.Text = "Ej: 12345678";
+        }
+    }
+}
--- BeLife/Vistas/Clientes.xaml.cs
+++ BeLife/Vistas/Clientes.xaml.cs
@@ -1,5 +1,4 @@
-﻿using BaseDatos.Controlador;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,21 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using BeLife.Vistas;
 
-namespace BeLife
+namespace BeLife.Vistas
 {
     /// <summary>
-    /// Lógica de interacción para MainWindow.xaml
+    /// Lógica de interacción para Clientes.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Clientes : Window
     {
-        public MainWindow()
+        public Clientes()
         {
             InitializeComponent();
-            
         }
 
         private void btn_salir_Click(object sender, RoutedEventArgs e)
@@ -35,7 +31,7 @@ namespace BeLife
 
         private void btn_clientes_Click(object sender, RoutedEventArgs e)
         {
-            Clientes ventana = new Clientes();
+            AdministrarCliente ventana = new AdministrarCliente();
             ventana.ShowDialog();
         }
     }
