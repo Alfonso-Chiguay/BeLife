b51f6934c220df3ae3d5e404a5538e53ab958dec
Adolfo Cerda Venegas <ado.cerda@alumnos.duoc.cl>
2020-07-12 13:24:58 -0400

    creo que se puede insertar correctamente en contrato(solo faltaria buscar los campos e intentar)

--- BaseDatos/Controlador/Con_Contrato.cs
+++ BaseDatos/Controlador/Con_Contrato.cs
@@ -8,5 +8,17 @@ namespace BaseDatos.Controlador
 {
     public class Con_Contrato
     {
+        public void contratoVehiculo(Contrato contrato, string idContrato, string patente)
+        {
+            using (BeLifeEntities entidades = new BeLifeEntities())
+            {
+                // CREO QUE ESTO AGREGA VALORES LA TABLA "FANTASMA" DE CONTRATO VEHICULO //
+                Contrato c = new Contrato(); //--CREO QUE ESTA TABLA ES INNECESARIA-->{Numero = idContrato};
+                c.Vehiculo.Add(new Vehiculo() { Patente = patente });
+                entidades.Contrato.Add(contrato);
+                entidades.Contrato.Add(c);
+                entidades.SaveChanges();
+            }
+        }
     }
 }
